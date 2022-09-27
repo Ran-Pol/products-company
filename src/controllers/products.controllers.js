5ed3424f6fe46abb8117fa1b38fcc45b5c481998
Ran Pol <rnd809@gmail.com>
2022-09-27 05:56:23 -0400

    Refactore the products routes and controlers into seperate module for import/exports

---
+++ src/controllers/products.controllers.js
@@ -0,0 +1,5 @@
+export const getProducts = (req, res) => {}
+export const createProduct = (req, res) => {}
+export const getProductById = (req, res) => {}
+export const updateProductById = (req, res) => {}
+export const deleteProductById = (req, res) => {}
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,5 +1,16 @@
 import { Router } from 'express'
+import * as productsCtrl from '../controllers/products.controllers'
 
 const router = Router()
+
+router.get('/', productsCtrl.getProducts)
+
+router.post('/', productsCtrl.createProduct)
+
+router.get('/:productId', productsCtrl.getProductById)
+
+router.delete('/:productId', productsCtrl.deleteProductById)
+
+router.put('/:productId', productsCtrl.updateProductById)
 
 export default router
