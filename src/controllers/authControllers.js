de65bbeed6c85d823f9128a7150a99f5d82b6637
Ran Pol <rnd809@gmail.com>
2022-09-28 05:43:15 -0400

    Created the tokens for the users at the time of sign up.

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,15 +1,22 @@
 import User from '../models/Users'
+import jwt from 'jsonwebtoken'
+import config from '../config'
 
 export const signup = async (req, res) => {
   const { username, email, password, roles } = req.body
-  
+
   const newUser = new User({
     username,
     email,
     password: await User.encryptPassword(password),
   })
   const savedUser = await newUser.save()
-  res.json(savedUser)
+
+  const token = jwt.sign({ id: savedUser._id }, config.SECRET, {
+    expiresIn: 86400, //24 hours
+  })
+
+  res.status(200).json({ token })
 }
 
 export const signin = async (req, res) => {
