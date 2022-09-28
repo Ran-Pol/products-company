9e59f9369f7854e9b9f871e1464aac7a3e7dbbcd
Ran Pol <rnd809@gmail.com>
2022-09-28 06:40:02 -0400

    Finalize authentication with bcrypt for password functionalities and jsonwebtoken for tokens.

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -29,5 +29,19 @@ export const signup = async (req, res) => {
 }
 
 export const signin = async (req, res) => {
-  res.json('signin')
+  const userFound = await User.findOne({ email: req.body.email }).populate(
+    'roles'
+  )
+
+  if (!userFound) return res.status(400).json({ Message: 'User not found' })
+
+  const matchPassword = await User.comparePassword(
+    req.body.password,
+    userFound.password
+  )
+  if (!matchPassword)
+    return res.status(401).json({ token: null, message: 'Invalid password' })
+
+  const token = jwt.sign({ id: userFound._id }, config.SECRET, { expiresIn: 86400 })
+  res.json({ token })
 }
