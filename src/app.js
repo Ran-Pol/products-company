0a9c0b87698fe0886629f129a974715b93d9dcad
Ran Pol <rnd809@gmail.com>
2022-09-28 06:12:55 -0400

    Assign the roles of users at signup time. If the users adds roles we search and add the roles ids otherwise we assign a basic role.

--- src/app.js
+++ src/app.js
@@ -2,10 +2,14 @@ import express from 'express'
 import morgan from 'morgan'
 import pkg from '../package.json'
 
+import { createRoles } from './libs/initialSetUp'
+
 import productsRoutes from './routes/products.routes'
 import authRoutes from './routes/auth.routes'
 
 const app = express()
+createRoles()
+
 app.set('pkg', pkg)
 
 app.use(express.json())
--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,6 +1,7 @@
 import User from '../models/Users'
 import jwt from 'jsonwebtoken'
 import config from '../config'
+import Role from '../models/Roles'
 
 export const signup = async (req, res) => {
   const { username, email, password, roles } = req.body
@@ -10,6 +11,14 @@ export const signup = async (req, res) => {
     email,
     password: await User.encryptPassword(password),
   })
+  if (roles) {
+    const foundRoles = await Role.find({ name: { $in: roles } })
+    newUser.roles = foundRoles.map((role) => role._id)
+  } else {
+    const role = await Role.findOne({ name: 'user' })
+    newUser.roles = [role._id]
+  }
+
   const savedUser = await newUser.save()
 
   const token = jwt.sign({ id: savedUser._id }, config.SECRET, {
