18cf9d03d5f77932f671f1731e948d4e78890b13
Ran Pol <rnd809@gmail.com>
2022-09-29 01:27:16 -0400

    Finalize the REST API that oncoporates: User Authentication, and Routes protections.

--- src/middlewares/verifySignup.js
+++ src/middlewares/verifySignup.js
@@ -1,9 +1,28 @@
 import { ROLES } from '../models/Roles'
+import Users from '../models/Users'
+
+export const checkDuplicateUsernameOrEmail = async (req, res, next) => {
+  const userName = await Users.findOne({ username: req.body.username })
+
+  if (userName)
+    return res.status(400).json({
+      message: 'The username already exists',
+    })
+
+  const userEmail = await Users.findOne({ email: req.body.email })
+
+  if (userEmail)
+    return res.status(400).json({
+      message: 'The email already exists',
+    })
+
+  next()
+}
 
 export const checkRolesExisted = async (req, res, next) => {
   if (req.body.roles) {
     for (let i = 0; i < req.body.roles.length; i++) {
-      if (!ROLES.includes(req.nody.roles[i])) {
+      if (!ROLES.includes(req.body.roles[i])) {
         return res.status(400).json({
           message: `Role ${req.body.roles[i]} does not exists`,
         })
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,8 +2,13 @@ import { Router } from 'express'
 const router = Router()
 
 import * as authCtrl from '../controllers/authControllers'
+import { verifySignup } from '../middlewares'
 
-router.post('/signup', authCtrl.signup)
+router.post(
+  '/signup',
+  [verifySignup.checkDuplicateUsernameOrEmail, verifySignup.checkRolesExisted],
+  authCtrl.signup
+)
 router.post('/signin', authCtrl.signin)
 
 export default router
